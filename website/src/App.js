60644b08ed2c2b8af6e3a1f1897d876f60de7aed
Firzus <lprieu@gaming.tech>
2022-04-13 11:26:20 +0200

    routes

--- website/src/App.js
+++ website/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Routes , Route } from "react-router-dom";
 import Home from "./pages/Home";
+import NotFound from './pages/NotFound';
+import CustomerArea from './pages/CustomerArea';
+import OrderCard from './pages/OrderCard';
 
 const App = () => {
   return (
       <BrowserRouter>
         <Routes>
           <Route path="/" exact element={<Home />} />
+          <Route path="/commander-sa-carte" exact element={<OrderCard />} />
+          <Route path="/espace-client" exact element={<CustomerArea />} />
+          <Route element={<NotFound />} />
         </Routes>
       </BrowserRouter>
   );
