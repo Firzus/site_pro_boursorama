da58480ffb4c764739e8d3e92b60282d5e189d4e
Firzus <lprieu@gaming.tech>
2022-04-13 11:16:59 +0200

    routes

--- website/src/App.js
+++ website/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { BrowserRouter, Routes , Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <div>
-
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+        </Routes>
+      </BrowserRouter>
   );
 };
 
